705d989f95b9e03f9fe6d8dfd2ca244e73a4c905
Vladislav1992 <kuryanov.vlad@yandex.ru>
2022-12-27 03:21:57 +0300

    8. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,12 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+
+    LocalDateTime created = LocalDateTime.now();
 
     public Item() {
 
@@ -10,6 +14,10 @@ public class Item {
 
     public Item(String name) {
         this.name = name;
+    }
+
+    public LocalDateTime getTime() {
+        return this.created;
     }
 
     public Item(int id, String name) {
---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item contraption = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String currentDateTimeFormat = contraption.getTime().format(formatter);
+        System.out.println(currentDateTimeFormat);
+    }
+}
