1e58c95c962077bf8e9a00e370434496aef8f80a
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-01-01 12:28:17 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,26 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book code = new Book("Clean code", 464);
+        Book boule = new Book("Boule de Suif", 18);
+        Book sodom = new Book("The 120 Days of Sodom, or the School of Libertinage", 391);
+        Book sex = new Book("Sexual Behavior in the Human Male", 824);
+        Book[] books = new Book[4];
+        books[0] = code;
+        books[1] = boule;
+        books[2] = sodom;
+        books[3] = sex;
+        for (int index = 0; index < books.length; index++) {
+            Book pr = books[index];
+            System.out.println(pr.getName() + " - " + pr.getPages());
+        }
+        for (int index = 0; index < books.length; index++) {
+            Book pr = books[index];
+            System.out.println();
+            if (pr.getName() == "Clean code") {
+                System.out.println(pr.getName() + " - " + pr.getPages());
+            }
+        }
+    }
+}
