18f8a00d39b72bd4240e14d48863c8f4dd17ab2a
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-01-09 11:38:00 +0300

    9. Правила Checkstyle [#504981 #378836]

--- src/main/java/ru/job4j/checkstyle/Broken.java
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -2,7 +2,7 @@ package ru.job4j.checkstyle;
 
 public class Broken {
     public static final String NEW_VALUE = "";
-    public String surname;
+    private String surname;
     private String name;
     private int sizeOfEmpty = 10;
 
