944443c04cb6e28b1b86e9233e9e44308f51d0e6
Vladislav1992 <kuryanov.vlad@yandex.ru>
2023-02-04 21:45:14 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #388235]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -24,6 +24,6 @@ public class PhoneDictionaryTest {
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
         ArrayList<Person> persons = phones.find("Vlad");
-        assertThat(persons.isEmpty()).isEqualTo(true);
+        assertThat(persons.isEmpty()).isTrue();
     }
 }
