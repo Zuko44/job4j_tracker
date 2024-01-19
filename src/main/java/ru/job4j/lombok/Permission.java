bd008aed5055cdc6bf9c954490ebb510359927fb
Vladislav1992 <kuryanov.vlad@yandex.ru>
2024-01-19 19:47:13 +0300

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/lombok/Permission.java
@@ -0,0 +1,16 @@
+package ru.job4j.lombok;
+
+import lombok.Builder;
+import lombok.Singular;
+import lombok.ToString;
+
+import java.util.List;
+
+@Builder(builderMethodName = "of")
+@ToString
+public class Permission {
+    private int id;
+    private String name;
+    @Singular("accessBy")
+    private List<String> rules;
+}
